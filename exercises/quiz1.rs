08656ce79f9eee839c6702ee3d199b3bed2e2491
Angela Cortecchia <angela.cortecchia@hotmail.com>
2023-03-23 21:39:34 +0100

    did 22% of exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,17 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    const PRICE: i32 = 2;
+    const DISCOUNT: i32 = 1;
+    if (apples > 40){
+        apples * DISCOUNT
+    }else{
+        apples * PRICE
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
